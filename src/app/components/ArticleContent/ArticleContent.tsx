bc05f152cea50351dcaa5ae4741ba25b1da1974e
Lucy Wirth <lucywirth@gmail.com>
2024-06-17 16:54:45 +1000

    additional styling

--- src/app/components/ArticleContent/ArticleContent.tsx
+++ src/app/components/ArticleContent/ArticleContent.tsx
@@ -11,8 +11,9 @@ const data: any = articleData;
         <section className={styles.ArticleContent}>
       <h1> {articleData.pageProps.items.title}</h1> 
       <p> {articleData.pageProps.items.description}</p>     
-          <h2>{articleData.pageProps.items.content[0].fields.internal}</h2> 
-            {articleData.pageProps.items.content[3].fields.header}
+          <h2>{articleData.pageProps.items.content[0].fields.internal}</h2>
+          <p>Sometimes, having more than one person working on a task can get the job done much quicker. Assigning actions to users and groups allows the team to fulfill projects or maintenance tasks faster within the organization.</p>
+           <h2> {articleData.pageProps.items.content[3].fields.header}</h2> 
             </section>
     </div>
   )
